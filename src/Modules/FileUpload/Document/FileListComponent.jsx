68631f2eedb93c5b343180bc13547e6856b3b6f7
ManagerTMC <itmanager@tmc.ac.in>
2024-10-17 19:18:24 +0530

    file

--- src/Modules/FileUpload/Document/FileListComponent.jsx
+++ src/Modules/FileUpload/Document/FileListComponent.jsx
@@ -4,35 +4,35 @@ import { memo } from "react";
 import ClearIcon from "@mui/icons-material/Clear";
 
 const FileListComponent = ({
-  FileLink,
-  imageName,
-  handleRemoveFile,
-  uploadFIleSize,
+    FileLink,
+    imageName,
+    handleRemoveFile,
+    uploadFIleSize,
 }) => {
-  return (
-    <Box className="flex w-full border h-auto p-1 rounded-md">
-      <img
-        alt="upload image"
-        src={FileLink}
-        width={60}
-        height={"auto"}
-        className="border p-1 rounded-md"
-      />
-      <Box className="flex flex-grow flex-col px-2  justify-center text-wrap capitalize">
-        <Typography level="body-sm" fontWeight={900} fontSize={13}>
-          {imageName?.toLowerCase()}
-        </Typography>
-        <Typography level="body-xs" fontWeight={500} fontSize={10}>
-          {uploadFIleSize}
-        </Typography>
-      </Box>
-      <Box className="flex items-center px-3">
-        <IconButton color="danger" onClick={handleRemoveFile}>
-          <ClearIcon fontSize="large" />
-        </IconButton>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box className="flex w-full border h-16 p-1 rounded-md">
+            <img
+                alt="upload image"
+                src={FileLink}
+                width={60}
+                height={60}
+                className="border p-1 rounded-md object-contain"
+            />
+            <Box className="flex flex-grow flex-col px-2  justify-center text-wrap capitalize">
+                <Typography level="body-sm" fontWeight={900} fontSize={13}>
+                    {imageName?.toLowerCase()}
+                </Typography>
+                <Typography level="body-xs" fontWeight={500} fontSize={10}>
+                    {uploadFIleSize}
+                </Typography>
+            </Box>
+            <Box className="flex items-center px-3">
+                <IconButton color="danger" onClick={handleRemoveFile}>
+                    <ClearIcon fontSize="large" />
+                </IconButton>
+            </Box>
+        </Box>
+    );
 };
 
 export default memo(FileListComponent);
